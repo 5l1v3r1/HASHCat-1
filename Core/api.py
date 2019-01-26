578221da19f0c67ac9db3eae3b7dbdca6a50340b
root <root@localhost.localdomain>
2019-01-26 04:33:20 +0000

    Update Version #3.0

--- Core/api.py
+++ Core/api.py
@@ -1,22 +1,58 @@
-import json
+import requests,re
 
-import requests
+class hcat:
+ def nitrxgen(self,HASH):
+    response = requests.get('http://www.nitrxgen.net/md5db/' + HASH).text
+    if response:
+        return response
+    else:
+        return False
 
-from exceptions import *
+ def hashtoolkit(self,HASH):
+     response = requests.get("http://hashtoolkit.com/reverse-hash?hash=" + HASH).content
+     try:
+        cracked = re.findall("<span title=\"decrypted (md5|sha1|sha224|sha256|sha384|sha512) hash\">(.*)</span>", response)
+	return cracked[0][1]
+     except Exception:
+	return False
 
-def api_request(path, base_url="https://lea.kz"):
-    try:
-        response = requests.get(base_url + path)
-        if response.status_code == 404:
-            raise LeakzNotLeaked
-        return response.json()
-    except requests.exceptions.RequestException:
-        raise LeakzRequestException
-    except json.decoder.JSONDecodeError as error:
-        raise LeakzJSONDecodeException
+ def leakz(self,HASH):
+    response = requests.get('https://lea.kz/api/hash/' + HASH)
+    if response:
+        return response.json()['password']
+    else:
+        return False
 
+ def md5decrypt(self,HASH):
+	if len(HASH) ==32:
+		hashType = 'md5'
+	elif len(HASH) == 40:
+		hashType = 'sha1'
+	elif len(HASH) == 56:
+		return False
+	elif len(HASH) ==64:
+		hashType = 'sha256'
+	elif len(HASH) == 96:
+		hashType = 'sha384'
+	elif len(HASH) ==128:
+		hashType = 'sha512'
 
+	if hashType =='md5':
+		response = requests.get('http://md5decrypt.net/Api/api.php?hash={}&hash_type={}&email=deanna_abshire@proxymail.eu&code=1152464b80a61728'.format(HASH, hashType)).text
+		if len(response) != 0:
+			return response
+		else:
+		  response = requests.get('http://md5decrypt.net/Api/api.php?hash={}&hash_type={}&email=deanna_abshire@proxymail.eu&code=1152464b80a61728'.format(HASH, 'md4')).text
+		  if len(response) !=0:
+			return response
+		  else:
+		     return False
+        else:
+           response = requests.get('http://md5decrypt.net/Api/api.php?hash={}&hash_type={}&email=deanna_abshire@proxymail.eu&code=1152464b80a61728'.format(HASH, hashType)).text
+	   if len(response) !=0:
+		return response
+	   else:
+		return False
 
-def ONCH(hash):
-    return api_request("/api/hash/" + hash).get("password")
+hcat = hcat()
 
--- HASHCat.py
+++ HASHCat.py
@@ -5,10 +5,11 @@
 #------------------------------#
 try:
    from os import system
-   system("")
+   system("cls||clear")
    import hashlib,optparse,re,socket
    from time import sleep
    from copy import copy
+   from sys import stdout as std
 except ImportError as e:
 	e = e[0][16:]
 	print("\033[1;31m[\033[1;33m!\033[1;31m]\033[33m Error: Module[\033[1;37m{}\033[1;33m] Not Found \033[1;31m!!!\n\033[1;37m[\033[1;32m*\033[1;37m]\033[1;33m Please install it :)".format(e))
@@ -20,14 +21,14 @@ except ImportError as e:
 try:													   #
    from Core.banner import banner,colors,cor								   #
    from Core.examples import examples									   #
-except ImportError:													   #
+except ImportError:											   #
 	print(cor[3]+"\n[!]:"+cor[5]+"The ["+cor[4]+" Core"+cor[5]+" ]"+cor[3]+"Tool Folder Is Missing!!") #
 	exit(1)												   #
 ############################################################################################################
 
 #----------------------== VERSION ==-----------------------#
 							   #
-ver = cor[5]+"\nTool Version: [ "+cor[2]+"2.5"+cor[5]+" ]" #
+ver = cor[5]+"\nTool Version: [ "+cor[2]+"3.0"+cor[5]+" ]" #
 							   #
 #----------------------------------------------------------#
 
@@ -41,7 +42,7 @@ def errorhash():									      #
 --------------------------------------------->SHA224
 --------------------------------------------->SHA256
 --------------------------------------------->SHA384
---------------------------------------------->SHA512 """)										      #
+--------------------------------------------->SHA512                                           """)										              #
      pass		         							      #
 ###############################################################################################
 
@@ -52,7 +53,7 @@ ___  / / /__    |_  ___/__  / / /_  ____/_____ ___  __/
 __  /_/ /__  /| |____ \__  /_/ /_  /    _  __ `/_  /   
 _  __  / _  ___ |___/ /_  __  / / /___  / /_/ /_  /    
 /_/ /_/  /_/  |_/____/ /_/ /_/  \____/  \__,_/ /_/     
-                                                 V[2.5]
+                                                 V[3.0]
 """
 #-------------------------------------------------------------------------------------#
 
@@ -64,6 +65,8 @@ parse = optparse.OptionParser(defaultBanner +cor[5]+ """Usage: python ./HASHCat.
 +=========================================================================================================+
     -H <Multi Hash> -W <Wordlist>  |  Brute Force Attack On HASHES[MD5,SHA1,SHA224,SHA256,SHA384,SHA512]  |
 				   +									  +
+    -F <HASHES FILE> -W <Wordlist> |  Brute Force Attack On All Hash In File 				  |
+                                   +									  +
     -H <Multi Hash> --ON	   |  Online Cracking Hash Without Wordlist				  |
 				   +									  +
     -H <HashName> -T <Set Text>    |  Encode Your Text To HASHES[MD5,SHA1,SHA224,SHA256,SHA384,SHA512]	  |
@@ -89,18 +92,15 @@ def main():
         parse.add_option("-T","-t","--text",'--TEXT',dest="dtext",type="string",
 							        help="Enter Some Text To Encodeing To Your Hash\n")
 	parse.add_option("--on",'--ON','--On','--online','--ONLINE',action="store_true",dest="ONC",default=False)
-
-        parse.add_option('-f','-F','--file','--FILE',dest='Cfile',type='string')
-
+        parse.add_option('-f','-F','--file','--FILE',dest='Cfile',type='string',
+								help="Enter Hashes File")
 	parse.add_option("-O","-o","--output",'--OUTPUT',dest="outfile",type="string",
 								help="\nFile Name Please !\n")
 	parse.add_option("-v","-V",action="store_true",dest="ShowVERSION",default=False,
 								help="\nSHOW TOOL VERSION AND EXIT :)\n")
 	parse.add_option("-e","-E","--examples",'--EXAMPLES',action="store_true",dest="ShowEXAMPLES",default=False,
 								help="\nSHOW TOOL EXAMPLES\n")
-
 	(options,args) = parse.parse_args()
-
 #=-=-=-=-=-=-=-=-=-=-=-=-=- START =-=-=-=-=-=-=-=-=-=-=-=-=-:)
 
 	if options.ShowVERSION:
@@ -120,7 +120,7 @@ def main():
 		pass
 	  return False
 
-	check = checkoutfile()
+	checkfile = checkoutfile()
 
 	if options.hash !=None and options.wordlist !=None:
 		hash = options.hash
@@ -177,8 +177,9 @@ def main():
 				break
 # START BRUTE-FORCE ATTACK:...
 			   if hash == hashcat:
-				if check == True:
+				if checkfile == True:
 					 outfile = options.outfile
+                                         if outfile[-4:] !=".txt" : outfile = outfile+".txt"
 					 file = open(outfile, "w")
 					 file.write("\n[+]:HASH CRACK!:\n[*]:INFO:\n----------\n[*]:HASH Name:[ "+name+" ]\n[+]:HASH:[ "+hash+" ]\n\n[#>]:HASH CAT:[ "+passwd.strip()+" ]\n")
 					 print(cor[1]+"\n[+]"+cor[4]+":"+cor[2]+"HASH CRACKER: AND The INFO Save In [ "+cor[4]+outfile+cor[2]+" ] output file :)\n\033[1;32m[\033[1;37m*\033[1;32m] \033[1;37mCheck Out\033[1;32m :)")
@@ -193,6 +194,7 @@ def main():
 			       loop +=1
 		           lines+=1
 		   else:
+
 			print(cor[4]+"\n[-]:"+cor[3]+"I WAS TRY ["+cor[4]+str(lines)+cor[3]+"] "+cor[3]+"Passowrd From [ "+cor[4]+wl+cor[3]+" ] Wordlist")
 			print(cor[4]+"[x!]"+cor[3]+":PASSWORD NOT FOUND IN:[ "+cor[4]+wl+cor[3]+" ] Wordlist\n"+cor[2]+"[*]"+cor[4]+":"+cor[3]+"Try Online Cracker Options Or try Other Wordlist :) ")
 			exit(1)
@@ -202,7 +204,66 @@ def main():
 			       sleep(2)
 			       print("Bye :)")
 			       exit(1)
-
+	elif options.Cfile !=None and options.wordlist !=None:
+               hashfile = options.Cfile
+	       wordlist = options.wordlist
+	       try:
+                  hop = open(hashfile, "r")
+                  hop.close()
+               except IOError:
+		   print(cor[4]+"["+cor[3]+"!"+cor[4]+"]"+cor[3]+" No such file or directory: "+cor[5]+hashfile+cor[4]+" !!!")
+  		   exit(1)
+	       try:
+                  wop = open(wordlist, "r")
+                  wop.close()
+               except IOError:
+	           print(cor[4]+"["+cor[3]+"!"+cor[4]+"]"+cor[3]+" No such file or directory: "+cor[5]+wordfile+cor[4]+" !!!")
+  		   exit(1)
+               loop = 1
+               system("cls||clear")
+               banner()
+	       print(colors + "\n[+>]<====================> CONFIG <====================>[<+]\n"+cor[5])
+	       print("\033[1;32m[\033[1;37m*\033[1;32m]\033[1;37m HASH FILE    :> [ \033[1;32m"+hashfile)
+	       print("\033[1;32m[\033[1;37m>\033[1;32m]\033[1;37m Wordlist     :> [ \033[1;32m"+wordlist)
+	       sleep(1.2)
+	       print(cor[2]+"\n[*]"+cor[5]+" Brute Forcing: "+cor[2]+"Enable"+cor[5]+"...\n")
+	       sleep(1.7)
+	       s = 0
+	       with open(hashfile) as hop:
+                 for H in hop:
+		    if not H.strip(): continue
+                    H = H.strip()
+		    if len(H) not in [32,40,56,64,96,128]: continue
+		    print("\n[*] Cracking [{}]...".format(H))
+                    with open(wordlist) as wop:
+			for P in wop:
+			  if not P.strip(): continue
+			  P = P.strip()
+			  if len(H) == 32: #MD5
+			      name = "MD5"
+			      hashcat = hashlib.md5(P).hexdigest()
+			  elif len(H) == 40: #SHA1
+			      name = "SHA1"
+		              hashcat = hashlib.sha1(P).hexdigest()
+		          elif len(H) == 56: #SHA224
+			      name = "SHA224"
+		              hashcat = hashlib.sha224(P).hexdigest()
+		          elif len(H) == 64: #SHA256
+			      name = "SHA256"
+		              hashcat = hashlib.sha256(P).hexdigest()
+		          elif len(H) == 96: #SHA384
+			      name = "SHA384"
+		              hashcat = hashlib.sha384(P).hexdigest()
+		          elif len(H) == 128: #SHA512
+			      name = "SHA512"
+		              hashcat = hashlib.sha512(P).hexdigest()			  
+			  if hashcat ==H:
+			    s+=1
+			    print("  [+] HASH CRACK [+]\n  --------------\n    [N] HASH-NAME:>[ {}\n    [H] HASH     :>[ {}\n    [P] HASH CAT :>[ {}\n".format(name,H,P))
+			if s ==0:
+			   print("  [!] Password Not Found In Wordlist[{}] !!!".format(wordlist))
+		    s = 0
+	            print("=============================")
 	elif options.hash !=None and options.dtext !=None:
 		HASH = options.hash
 		text = options.dtext
@@ -230,8 +291,9 @@ def main():
                           hashte = hashlib.sha384(text).hexdigest()
                    elif HASH == "sha512" or HASH == "SHA512":
                           hashte = hashlib.sha512(text).hexdigest()
-		   if check == True:
+		   if checkfile == True:
 			outfile = options.outfile
+                        if outfile[-4:] !=".txt" : outfile = outfile+".txt"
 			file = open(outfile, "w")
 			file.write("\n[*] ======== [{}]HASH Result========[*]\n |\n[T] Your Plain Text    :> [ {} \n[H] Hash Name Type     :> [ {} \n[E] {}-HASH.[ {} ] :> [ {} \n |\n[*]===================================[*]\n".format(HASH.upper(),text,copy(HASH.upper()),copy(HASH.upper()),copy(text),hashte))
 			print("\n\033[1;32m[\033[1;37m*\033[1;32m] \033[1;37mYour Hashing Result Is Saved In :[\033[1;32m {}\033[1;37m ]\033[1;33m Output File. \n\033[1;32m[\033[1;37m*\033[1;32m]\033[1;37m Check Out \033[1;32m:)".format(outfile))
@@ -249,14 +311,8 @@ def main():
 	           print("\n\033[1;31m[\033[1;33m!\033[1;31m]\033[1;33m Error: [\033[1;32m Requests \033[1;37mLibrary\033[1;33m ] Is Not Exist \033[1;31m!!!")
 	           print("\033[1;32m[\033[1;37m*\033[1;32m]\033[1;37m Please Install It Using This \033[1;33mCommand: \033[1;37mpip install requests")
 		   exit(1)
-              try:
-                import json
-              except ImportError:
-	           print("\n\033[1;31m[\033[1;33m!\033[1;31m]\033[1;33m Error: [\033[1;32m Json \033[1;37mLibrary\033[1;33m ] Is Not Exist \033[1;31m!!!")
-	           print("\033[1;32m[\033[1;37m*\033[1;32m]\033[1;37m Please Install It Using This \033[1;33mCommand: \033[1;37mpip install simplejson")
-		   exit(1)
 	      try:
-		 from Core.api import ONCH as onc
+		 from Core.api import hcat
 	      except ImportError:
 		 print("\n\033[1;31m[\033[1;33m!\033[1;31m]\033[1;33m Error: [\033[1;37m Core/api.py \033[1;33m] File Is Missing \033[1;31m !!!")
 		 exit(1)
@@ -266,7 +322,7 @@ def main():
 		HASHES = [32,40,56,64,96,128]
 		if len(H) not in HASHES:
 			errorhash()
-			stop+=1
+			exit(1)
 		def check():
 		  try:
 		    ip = socket.gethostbyname('google.com')
@@ -276,34 +332,8 @@ def main():
 			pass
 		  return False
 		if check() !=True:
-			stop +=1
 			print("\n\033[1;31m[\033[1;33m!\033[1;31m]\033[1;33m Error: Please Check Your Internet Connection \033[1;31m!!!")
 			exit(1)
-		url="http://hashtoolkit.com/reverse-hash?hash="+H
-		con = requests.get(url)
-		content = con.content
-		cracked = re.findall("<span title=\"decrypted (md5|sha1|sha224|sha256|sha384|sha512) hash\">(.*)</span>", content)
-		hashname=cracked[0][0]
-		banner()
-		print("\033[1;37m.::: \033[1;33mOnline Cracking Result \033[1;37m:::.")
-		print("\n\033[1;37m[\033[1;32m>\033[1;37m] HASH     :> [\033[1;32m "+H[:30]+"\033[1;33m-...")
-		print("\033[1;37m[\033[1;32m+\033[1;37m] HashName :> [\033[1;32m "+hashname.upper())
-		print("\n\033[1;37m[\033[1;32m~\033[1;37m] HASH CAT :> [\033[1;32m "+cracked[0][1])
-	      except:
-		if stop >0:
-		   exit(1)
-                H = options.hash
-                def check():
-                  try:
-                    ip = socket.gethostbyname('google.com')
-                    con = socket.create_connection((ip, 80), 2)
-                    return True
-                  except:
-                        pass
-                  return False
-                if check() !=True:
-                        print("\n\033[1;31m[\033[1;33m!\033[1;31m]\033[1;33m Error: Please Check Your Internet Connection \033[1;31m!!!")
-                        exit(1)
 		if len(H) ==HASHES[0]:
 			name = "MD5"
 		elif len(H) ==HASHES[1]:
@@ -317,13 +347,32 @@ def main():
                 elif len(H) ==HASHES[5]:
                         name = "SHA512"
 		banner()
-                print("\033[1;37m .::: \033[1;33mOnline Cracking Result \033[1;37m:::.")
+                print("\033[1;37m .::: \033[1;33mOnline Cracking \033[1;37m:::.")
                 print("\n\033[1;37m[\033[1;32m>\033[1;37m] HASH     :> [\033[1;32m "+H[:30]+"\033[1;33m-...")
                 print("\033[1;37m[\033[1;32m+\033[1;37m] HashName :> [\033[1;32m "+name.upper())
-		try:
-                 print("\n\033[1;37m[\033[1;32m~\033[1;37m] HASH CAT :> [\033[1;32m "+onc(H))
-		except:
-		  print("\n\033[1;31m[\033[1;33m!\033[1;31m]\033[1;33m Error: Cracking Failed \033[1;31m !!!\n\033[1;37m[\033[1;32m*\033[1;37m] Try Brute Force Attack With Wordlist :)")
+
+                cracked = hcat.nitrxgen(H)
+		if cracked == False:
+		     cracked = hcat.hashtoolkit(H)
+		     if cracked == False:
+		         cracked = hcat.leakz(H)
+			 if cracked == False:
+		            cracked = hcat.md5decrypt(H)
+		if cracked !=False:
+		  if checkfile == True:
+			outfile = options.outfile
+                        if outfile[-4:] !=".txt" : outfile = outfile+".txt"
+			fop = open(outfile, 'w')
+			fop.write('\n[*] HASH CAT :> '+str(cracked)+"\n")
+			fop.close()
+			print("\n\033[1;32m[\033[1;37m*\033[1;32m] \033[1;37mYour Hash Online Crack Result Is Saved In :[\033[1;32m {}\033[1;37m ]\033[1;33m Output File. \n\033[1;32m[\033[1;37m*\033[1;32m]\033[1;37m Check Out \033[1;32m:)".format(outfile))
+		  else:
+                       print("\n\033[1;37m[\033[1;32m~\033[1;37m] HASH CAT :> [\033[1;32m "+str(cracked))
+		else:
+		  print("\n\033[1;31m[\033[1;33m!\033[1;31m]\033[1;33m Error: Online Cracking Failed \033[1;31m !!!\n\033[1;37m[\033[1;32m*\033[1;37m] Try Brute Force Attack With Wordlist :)")
+	      except Exception,e:
+		  print("\n[!] Error:"+str(e.message))
+		  exit(1)
 
 	elif options.hash !=None and options.Cfile !=None:   
              hashname = options.hash
@@ -380,6 +429,7 @@ def main():
                else:
                    errorhash()
                    exit(1)
+
              banner()
              sleep(0.10)
              print("\n"+cor[2]+"["+cor[0]+"*"+cor[2]+"]"+cor[4]+" Hash: "+cor[1]+hashname)
@@ -394,7 +444,7 @@ def main():
 
 if __name__=='__main__':
 	main()
-	
+
 ##############################################################
 #####################                #########################
 #####################   END OF TOOL  #########################
